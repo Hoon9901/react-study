9db6112191e0838856b62ab34daa0d4ca620bcfa
Hooooooon <shonn2323@gmail.com>
2021-07-30 23:46:53 +0900

    Study: useInput Hook 유효성검사 추가

--- hooks/src/hooks/one.js
+++ hooks/src/hooks/one.js
@@ -1,16 +1,27 @@
 import React, { useState } from "react";
 
 // eslint-disable-next-line
-const useInput = (initalValue) => {
+const useInput = (initalValue, validator) => {
   const [value, setValue] = useState(initalValue);
   const onChange = (event) => {
-    console.log(event.target);
+    // 객체 리터럴로 event의 target value 가져온다.
+    const {
+      target: { value },
+    } = event;
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+    if (willUpdate) {
+      setValue(value);
+    }
   };
   return { value, onChange };
 };
 
 function Fone() {
-  const name = useInput("Mr.");
+  const maxOfLength = (value) => value.includes("@");
+  const name = useInput("Mr.", maxOfLength);
   return (
     <div>
       <h1>Hello</h1>
